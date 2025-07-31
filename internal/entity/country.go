cf812298752669482c46538c594a68d156c8b772
ursulgwopp <ursuldm@gmail.com>
2025-07-31 20:51:24 +0300

    reviewed entities

--- internal/entity/country.go
+++ internal/entity/country.go
@@ -6,10 +6,3 @@ type Country struct {
 	Alpha3 string `db:"alpha3"`
 	Region string `db:"region"`
 }
-
-// type Country struct {
-// 	Name   string `json:"name" db:"name"`
-// 	Alpha2 string `json:"alpha2" db:"alpha2"`
-// 	Alpha3 string `json:"alpha3" db:"alpha3"`
-// 	Region string `json:"region" db:"region"`
-// }
